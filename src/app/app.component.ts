7c435f7de3204395fae5593afa6f7be0df1bfdba
melissa yauri <yaurimelissa@gmail.com>
2018-11-21 16:20:55 -0500

    inicializando proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'entrada-salida';
+  /* Propiedades */
+  nombre: string = "melissa";
+  correo:string;
+  /*lista en donde se guardara nombre mas correo */
+  listaDatos: {}[] = [];
+  mostrar: Boolean = true;
+  desabilitar:  Boolean = true;
+  /* mostrar = true */
+  /* Funciones */
+
+  /* Agregando nombre y correo */
+  agregar(){
+    console.log('click');
+    /* toma los valores de nombre y correo */
+    let detalleDatos = {nombre: this.nombre, correo: this.correo }
+    /* incorporando los valores en la lista */
+    this.listaDatos.push(detalleDatos);
+    console.log(this.listaDatos);
+    /*limpiando los campos, la propieda de ngModel */
+    this.nombre =" "
+    this.correo = " "
+  }
+
+  /* mostrar y ocultar correo */
+  ver(){
+    this.mostrar =! this.mostrar;
+    //console.log(most);
+    console.log(this.mostrar);
+   
+  }
 }
